f15e7b9d2fada67da950c061f9db303dc2d970e5
Eduardo Vital Alencar Cunha <vital@hey.com>
2020-07-11 19:08:37 -0300

    feat(macos): add convenience init to NSColor

---
+++ touch_bar_macos/macos/Classes/Extensions/NSColorExtension.swift
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Eduardo Vital Alencar Cunha
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+extension NSColor {
+    convenience init?(fromRGBA rgba: NSDictionary?) {
+      guard let red = rgba?["red"] as? CGFloat,
+            let green = rgba?["green"] as? CGFloat,
+            let blue = rgba?["blue"] as? CGFloat,
+            let alpha = rgba?["alpha"] as? CGFloat else {
+            return nil
+      }
+      self.init(deviceRed: red, green: green, blue: blue, alpha: alpha)
+  }
+}
