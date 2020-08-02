f1d101194bee69d96a0cc84458664895cad7343d
Eduardo Vital Alencar Cunha <vital@hey.com>
2020-08-02 16:50:49 -0300

    format(macos): fix NSColorExtension indentation

--- touch_bar_macos/macos/Classes/Extensions/NSColorExtension.swift
+++ touch_bar_macos/macos/Classes/Extensions/NSColorExtension.swift
@@ -3,13 +3,13 @@
 // found in the LICENSE file.
 
 extension NSColor {
-    convenience init?(fromRGBA rgba: NSDictionary?) {
-      guard let red = rgba?["red"] as? CGFloat,
-            let green = rgba?["green"] as? CGFloat,
-            let blue = rgba?["blue"] as? CGFloat,
-            let alpha = rgba?["alpha"] as? CGFloat else {
-            return nil
-      }
-      self.init(deviceRed: red, green: green, blue: blue, alpha: alpha)
+  convenience init?(fromRGBA rgba: NSDictionary?) {
+    guard let red = rgba?["red"] as? CGFloat,
+      let green = rgba?["green"] as? CGFloat,
+      let blue = rgba?["blue"] as? CGFloat,
+      let alpha = rgba?["alpha"] as? CGFloat else {
+      return nil
+    }
+    self.init(deviceRed: red, green: green, blue: blue, alpha: alpha)
   }
 }
