707a03460f56da46fccf7d4a710fcce843295ba4
Eduardo Vital Alencar Cunha <vital@hey.com>
2020-08-02 16:47:38 -0300

    feat(plugin): make TouchBarImage available

--- touch_bar/lib/touch_bar.dart
+++ touch_bar/lib/touch_bar.dart
@@ -6,6 +6,7 @@ import 'package:touch_bar_platform_interface/models/touch_bar.dart';
 import 'package:touch_bar_platform_interface/touch_bar_platform_interface.dart';
 
 export 'package:touch_bar_platform_interface/models/touch_bar.dart';
+export 'package:touch_bar_platform_interface/models/touch_bar_image.dart';
 export 'package:touch_bar_platform_interface/models/touch_bar_items/touch_bar_button.dart';
 export 'package:touch_bar_platform_interface/models/touch_bar_items/touch_bar_label.dart';
 
