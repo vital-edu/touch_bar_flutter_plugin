e2fb6a1516ed54fa1074eb7a3fef0546963d57aa
Eduardo Vital Alencar Cunha <vital@hey.com>
2020-08-15 20:27:43 -0300

    feat(example): add label update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,58 +28,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
+    Colors.pink,
+    Colors.purple,
+    Colors.grey,
+    Colors.orange,
+    Colors.brown,
+  ];
   int _counter = 0;
   TouchBar bar;
-  TouchBarImage icon;
+  TouchBarLabel counterLabel = new TouchBarLabel(
+    'Counter: 0',
+    textColor: Colors.red,
+  );
+  TouchBarLabel timesTwoCounterLabel = TouchBarLabel(
+    'Counter: 2',
+    textColor: Colors.blue,
+  );
 
   _incrementCounter() {
     setState(() {
       _counter++;
-      bar = TouchBar(children: [
-        TouchBarButton(
-          label: "Increment +1",
-          backgroundColor: Colors.yellow,
-          onClick: _incrementCounter,
-          icon: icon,
-        ),
-        TouchBarLabel('Red Label', textColor: Colors.red),
-        TouchBarLabel('Blue Label', textColor: Colors.blue),
-        TouchBarLabel('Counter: $_counter', textColor: Colors.white),
-        TouchBarPopover(
-          label: 'Options',
-          icon: icon,
-          iconPosition: ImagePosition.right,
-          children: [
-            TouchBarLabel('Yello Label', textColor: Colors.yellow),
-            TouchBarLabel('Pink Label', textColor: Colors.pink),
-            TouchBarButton(
-              label: "Increment +1",
-              backgroundColor: Colors.yellow,
-              onClick: _incrementCounter,
-              icon: icon,
-            ),
-          ],
-          showCloseButton: true,
-        ),
-      ]);
-      setTouchBar(bar);
+      counterLabel.label = 'Counter: $_counter';
+      counterLabel.textColor = colors[_counter % colors.length];
+      timesTwoCounterLabel.label = 'Counter: ${_counter * 2}';
+      timesTwoCounterLabel.textColor = colors[(_counter + 1) % colors.length];
     });
   }
 
   @override
   void initState() {
     super.initState();
-    loadAssetImage();
+    loadAssyncData();
   }
 
-  void loadAssetImage() async {
+  void loadAssyncData() async {
     final image = await TouchBarImage.loadFrom(
       path: 'assets/icons/plus.png',
       key: 'plus',
     );
 
+    bar = TouchBar(children: [
+      TouchBarButton(
+        label: "Increment +1",
+        backgroundColor: Colors.green,
+        onClick: _incrementCounter,
+        icon: image,
+        iconPosition: ImagePosition.left,
+      ),
+      TouchBarLabel('Red Label', textColor: Colors.red),
+      TouchBarLabel('Blue Label', textColor: Colors.blue),
+      counterLabel,
+      TouchBarPopover(
+        label: 'Options',
+        icon: image,
+        iconPosition: ImagePosition.left,
+        children: [
+          TouchBarLabel('Yello Label', textColor: Colors.yellow),
+          TouchBarLabel('Pink Label', textColor: Colors.pink),
+          timesTwoCounterLabel,
+          TouchBarButton(
+            label: "Increment +1",
+            backgroundColor: Colors.yellow,
+            onClick: _incrementCounter,
+            icon: image,
+            iconPosition: ImagePosition.right,
+          ),
+        ],
+        showCloseButton: true,
+      ),
+    ]);
+    setTouchBar(bar);
+
     setState(() {
-      this.icon = image;
+      this.bar = bar;
     });
   }
 
