c1a8bc1a7393a44c81853b5b5c3d3ad3393eb7cc
Bianca Amorim <amorim.bia.dev@gmail.com>
2021-03-25 20:39:40 -0300

    Add Authors file and change authors in the license file

--- touch_bar_platform_interface/lib/models/touch_bar.dart
+++ touch_bar_platform_interface/lib/models/touch_bar.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Eduardo Vital Alencar Cunha
+// Copyright (c) 2020 The TouchBar Flutter Plugin Authors
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 import 'package:flutter/widgets.dart';
